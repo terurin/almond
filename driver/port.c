bd36c8fbe631e7ef113c881b37765c798757d6bc
terurin <hamusutaateru@live.jp>
2018-05-24 15:57:39 +0900

    wrote peripheral assingment

--- driver/port.c
+++ driver/port.c
@@ -2,6 +2,7 @@
 #include <p33Fxxxx.h>
 #include <stddef.h>
 
+
 void pin_dir(pin_name_t pin, bool dir) {
     port_name_t port = pin_get_port(pin);
     uint16_t id = pin_get_id(pin);
@@ -75,5 +76,21 @@ void pin_clr(pin_name_t pin){
             return;
         default:
             return ;
+    }
+}
+
+void ppso_assing(pin_name_t pin,ppso_name_t ppso){
+    uint16_t pid = pin_get_pid(pin);
+    uint8_t* RPRn = (uint8_t*)&RPOR0;//起点を取得
+    if (pid<26){//RP25まで存在する(データシートより)
+        RPRn[pid]=ppso;
+    }
+}
+
+void ppsi_assing(pin_name_t pin,ppsi_name_t ppsi){
+    uint8_t* RPRn=(uint8_t*)&RPINR0;
+    uint16_t idx =(uint16_t) ppsi;
+    if (idx<(uint16_t)PPSI_END){
+        RPRn[idx]= pin_get_pid(pin);
     }
 }
--- driver/port.h
+++ driver/port.h
@@ -2,87 +2,130 @@
  * 用途　Portの管理
  */
 
-
 #ifndef DRIVER_PORT_HEADER_GUARD
 #define DRIVER_PORT_HEADER_GUARD
 
 #include <stdbool.h>
 #include <stdint.h>
 //全てのPinの名前の定義
+
 enum pin_name {
-    // 上位4bit ... port
-    // 下位4bit ... pin
-    PIN_HA = 0x19,
-    PIN_HB = 0x26,
-    PIN_HC = 0x27,
-    PIN_OTW = 0x28,
-    PIN_FALUT = 0x29,
-    PIN_PWM_C = 0x10,
-    PIN_RST_C = 0x11,
-    PIN_PWM_B = 0x12,
-    PIN_RST_B = 0x13,
+    //[0..3] ... pin
+    //[4..5] ... port
+    //[6..10] ...peripheral
+    //[11..15]
+
+    PIN_HA = 0x19 | 9 << 6,
+    PIN_HB = 0x26 | 22 << 6,
+    PIN_HC = 0x27 | 23 << 6,
+    PIN_OTW = 0x28 | 24 << 6,
+    PIN_FALUT = 0x29 | 25 << 6,
+    //VSS
+    //VCAP
+    PIN_PWM_C = 0x10 | 10 << 6,
+    PIN_RST_C = 0x11 | 11 << 6,
+    PIN_PWM_B = 0x12 | 12 << 6,
+    PIN_RST_B = 0x13 | 13 << 6,
     PIN_LED_A = 0x0A,
     PIN_LED_B = 0x07,
+    PIN_PWM_A = 0x1E | 14 << 6,
+    PIN_RST_A = 0x1F | 15 << 6,
     PIN_VREF = 0x00,
     PIN_RESERVE = 0x01,
-    PIN_PGD = 0x10,
-    PIN_PGC = 0x11,
-    PIN_VM = 0x12,
-    PIN_CA = 0x13,
-    PIN_CB = 0x20,
-    PIN_CC = 0x21,
+    PIN_PGD = 0x10 | 0 << 6,
+    PIN_PGC = 0x11 | 1 << 6,
+    PIN_VM = 0x12 | 2<<6,
+    PIN_CA = 0x13 | 3<<6,
+    PIN_CB = 0x20|16<<6,
+    PIN_CC = 0x21|16<<7,
     PIN_RESERVE2 = 0x22,
     PIN_CLKIN = 0x02,
     PIN_CLKOUT = 0x03,
     PIN_LED_C = 0x08,
-    PIN_CS = 0x14,
+    PIN_CS = 0x14|4<<6,
     PIN_HALT = 0x04,
     PIN_LED_D = 0x09,
-    PIN_MISO = 0x23,
-    PIN_MOSI = 0x24,
-    PIN_MCLK = 0x25,
-    PIN_RX = 0x15,
-    PIN_TX = 0x16,
-    PIN_QEI_Y = 0x17,
-    PIN_QEI_X = 0x18,
-    PIN_UNKNOWN= 0xFF
+    PIN_MISO = 0x23|19<<6,
+    PIN_MOSI = 0x24|20<<6,
+    PIN_MCLK = 0x25|21<<6,
+    PIN_RX = 0x15|5<<6,
+    PIN_TX = 0x16|6<<6,
+    PIN_QEI_Y = 0x17|7<<6,
+    PIN_QEI_X = 0x18|7<<6,
+    PIN_UNKNOWN = 0xFF
 };
 typedef enum pin_name pin_name_t;
 
-enum port_name{
-    PORT_A=0,
+enum port_name {
+    PORT_A = 0,
     PORT_B,
     PORT_C,
-    PORT_UNKNOWN=0xFF
+    PORT_UNKNOWN = 0xFF
 };
-
 typedef enum port_name port_name_t;
 
+//出力周辺機器IO
+enum ppso_name {
+    PPSO_NULL = 0b00000,
+    PPSO_U1TX = 0b00011,
+    PPSO_U1RTS = 0b00100,
+    PPSO_SDO1 = 0b00111,
+    PPSO_SCK1 = 0b01000,
+    PPSO_SS1OUT = 0b01001,
+    PPSO_OC1 = 0b10010,
+    PPSO_OC2 = 0b10011,
+    PPSO_UPDN = 0b11010
+};
+typedef enum ppso_name ppso_name_t;
+//入力周辺機器IO
+//参考　データーシート(p46)
+enum ppsi_name {
+    PPSI_INT1 = 0<<1|1,
+    PPSI_INT2 = 1<<1|0,
+    //timer
+    //input capture
+    PPSI_OC_FALUT_A = 11 << 1 | 0,
+    PPSI_PWM1_FALUT = 12 << 1 | 0,
+    PPSI_PWM2_FALUT = 13 << 1 | 0,
+    PPSI_QEIA = 14 << 1 | 0,
+    PPSI_QEIB = 14 << 1 | 1,
+    PPSI_QEI_INDEX = 15 << 1 | 0,
+    PPSI_U1RX = 18 << 1 | 0,
+    PPSI_U1CTS = 18 << 1 | 1,
+    PPSI_SDI = 20 << 1 | 0,
+    PPSI_SCK = 20 << 1 | 1,
+    PPSI_SS = 21 << 1|0,
+    PPSI_END = 21<<1|1//defect error
+};
+typedef enum ppsi_name ppsi_name_t;
 
-//プロパティ
-static inline port_name_t pin_get_port(pin_name_t pin){
-    return (pin&0xF0)>>4;
+//methods
+static inline port_name_t pin_get_port(pin_name_t pin) {
+    return ((uint16_t)pin & 0x0030) >> 4;
 }
 
-static inline uint16_t pin_get_id(pin_name_t pin){
-    return pin&0xF;
+static inline uint16_t pin_get_id(pin_name_t pin) {
+    return (uint16_t)pin & 0x000F;
+}
+//周辺機器ID
+static inline uint16_t pin_get_pid(pin_name_t pin){
+    return ((uint16_t)pin&0x07C0)>>6; 
 }
 
-void pin_dir(pin_name_t,bool);
+//basic pin function
+void pin_dir(pin_name_t, bool);
 bool pin_read(pin_name_t);
-void pin_set (pin_name_t);
+void pin_set(pin_name_t);
 void pin_clr(pin_name_t);
 
-static inline void pin_write(pin_name_t pin,bool out){
-    if (out){
-        pin_set(pin);
-    }else{
-        pin_clr(pin);
-    }
+static inline void pin_write(pin_name_t pin, bool out) {
+    out?pin_set(pin): pin_clr(pin);
 }
 
-
-
+//出力先変更
+void ppso_assing(pin_name_t pin,ppso_name_t ppso);
+//入力先変更
+void ppsi_assing(pin_name_t pin,ppsi_name_t ppsi);
 
 
 #endif
