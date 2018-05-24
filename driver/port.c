8d40ff16007eaa44da58b0c5e4684aaaa7e48782
terurin <hamusutaateru@live.jp>
2018-05-24 13:58:03 +0900

    wrote basic io

--- driver/port.c
+++ driver/port.c
@@ -1,4 +1,79 @@
 #include "port.h"
 #include <p33Fxxxx.h>
+#include <stddef.h>
 
+void pin_dir(pin_name_t pin, bool dir) {
+    port_name_t port = pin_get_port(pin);
+    uint16_t id = pin_get_id(pin);
+    uint16_t mask = 1u << id;
+    switch (port) {
+        case PORT_A:
+            TRISA = dir ? TRISA | mask : TRISA&~mask;
+            return;
+        case PORT_B:
+            TRISB = dir ? TRISB | mask : TRISB&~mask;
+            return;
+        case PORT_C:
+            TRISC = dir ? TRISC | mask : TRISC&~mask;
+            return;
+        default:
+            return;
+    }
+}
 
+bool pin_read(pin_name_t pin) {
+    port_name_t port = pin_get_port(pin);
+    uint16_t id = pin_get_id(pin);
+    uint16_t mask = 1u << id;
+    
+    switch (port){
+        case PORT_A:
+            return PORT_A&mask;
+        case PORT_B:
+            return PORT_B&mask;
+        case PORT_C:
+            return PORT_C&mask;
+        default:
+            return false;
+    }
+}
+
+void pin_set (pin_name_t pin){
+    port_name_t port = pin_get_port(pin);
+    uint16_t id = pin_get_id(pin);
+    uint16_t mask = 1u << id;
+    
+    switch (port){
+        case PORT_A:
+            LATA|=mask;
+            return;
+        case PORT_B:
+            LATB|=mask;
+            return ;
+        case PORT_C:
+            LATB|=mask;
+            return;
+        default:
+            return ;
+    }
+}
+
+void pin_clr(pin_name_t pin){
+    port_name_t port = pin_get_port(pin);
+    uint16_t id = pin_get_id(pin);
+    uint16_t mask = 1u << id;
+    
+    switch (port){
+        case PORT_A:
+            LATA&=~mask;
+            return;
+        case PORT_B:
+            LATB&=~mask;
+            return ;
+        case PORT_C:
+            LATB&=~mask;
+            return;
+        default:
+            return ;
+    }
+}
--- driver/port.h
+++ driver/port.h
@@ -1,6 +1,13 @@
+/* 作成　川上　輝
+ * 用途　Portの管理
+ */
+
+
 #ifndef DRIVER_PORT_HEADER_GUARD
 #define DRIVER_PORT_HEADER_GUARD
 
+#include <stdbool.h>
+#include <stdint.h>
 //全てのPinの名前の定義
 enum pin_name {
     // 上位4bit ... port
@@ -37,13 +44,42 @@ enum pin_name {
     PIN_RX = 0x15,
     PIN_TX = 0x16,
     PIN_QEI_Y = 0x17,
-    PIN_QEI_X = 0x18
+    PIN_QEI_X = 0x18,
+    PIN_UNKNOWN= 0xFF
 };
 typedef enum pin_name pin_name_t;
 
+enum port_name{
+    PORT_A=0,
+    PORT_B,
+    PORT_C,
+    PORT_UNKNOWN=0xFF
+};
+
+typedef enum port_name port_name_t;
 
 
+//プロパティ
+static inline port_name_t pin_get_port(pin_name_t pin){
+    return (pin&0xF0)>>4;
+}
 
+static inline uint16_t pin_get_id(pin_name_t pin){
+    return pin&0xF;
+}
+
+void pin_dir(pin_name_t,bool);
+bool pin_read(pin_name_t);
+void pin_set (pin_name_t);
+void pin_clr(pin_name_t);
+
+static inline void pin_write(pin_name_t pin,bool out){
+    if (out){
+        pin_set(pin);
+    }else{
+        pin_clr(pin);
+    }
+}
 
 
 
