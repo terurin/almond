e6abcb033f6008203f4bcb27e1c4bba7ac334391
terurin <hamusutaateru@live.jp>
2018-05-25 12:53:06 +0900

    sync branch

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/almond.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=almond.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=almond/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Almond.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Almond.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=almond.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/almond/bin
+makeDirectory ${TMPDIR}/almond.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/almond.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/almond.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/almond.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/almond.x.tar *
 checkReturnCode
 
 # Cleanup
